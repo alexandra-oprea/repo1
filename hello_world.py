0092040abacf00604e9830b16c46141287f861ba
alexandra-oprea <oprea.alexandra11@gmail.com>
2024-06-04 16:25:36 +0300

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# comentariu one line
+
+'''
+linia 1
+linia 2
+'''
+
+# printam in consola un mesaj
+print('Hello World!')
---
+++ variabile.py
@@ -0,0 +1,11 @@
+# variabila = zona din memoria calc care tine date
+# variabila = cutiuta in care punem valori
+
+# am declarat si initializat variabila marca
+marca_masina = 'Volvo'
+model_masina = 'XC 60'
+
+# nu putem sa punem spatiu in denumirea variabilei
+# o variabila incepe cu litera mica
+
+# loosely typed = nu trebuie sa specificam tipul de date
